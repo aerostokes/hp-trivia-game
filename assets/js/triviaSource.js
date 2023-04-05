d40198b0c5d84f063345fe23869c31b2266e6e99
Rhonda Stokes <rhonda@aerostokes.com>
2023-04-05 01:35:52 -0700

    add more trivia questions

--- assets/js/triviaSource.js
+++ assets/js/triviaSource.js
@@ -29,4 +29,64 @@ var triviaALL =[
         answer: "17",
         wrongs: ["19","10","15"]
     },
+    {
+        question: "What was James Potter's nickname when he was at Hogwarts?",
+        answer: "Prongs",
+        wrongs: ["Moony","Wormtail","Padfoot"]
+    },
+    {
+        question: "What is the password to the secret passageway to Honeydukes?",
+        answer: "Dissendium",
+        wrongs: ["Alohomora","Diffindo","Revlio"]
+    },
+    {
+        question: "Which victim of Slytherin's monster was found on Halloween night?",
+        answer: "Mrs. Norris",
+        wrongs: ["Colin Creevy","Justin Finch-Fletchley","Penelope Clearwater"]
+    },
+    {
+        question: "What is the title of Gilderoy Lockhart's autobiography?",
+        answer: "Magical Me",
+        wrongs: ["Gadding with Ghouls","Prefects Who Gained Power","Gilderoy The Savior"]
+    },
+    {
+        question: "What is Ron's Quidditch team?",
+        answer: "Chudley Cannons",
+        wrongs: ["Puddlemere United","Tutshill Tornados","Holyhead Harpies"]
+    },
+    {
+        question: "What is Lily Potter's patronus?",
+        answer: "Doe",
+        wrongs: ["Stag","Dog","Rabbit"]
+    },
+    {
+        question: "Where is the Divination classroom?",
+        answer: "North Tower",
+        wrongs: ["Astronomy Tower","Seventh Floor Corridor","Dungeons"]
+    },
+    {
+        question: "Which dragon did Fleur Delacour battle?",
+        answer: "Welsh Green",
+        wrongs: ["Chinese Fireball","Hungarian Horntail","Swedish Short-Snout"]
+    },
+    {
+        question: "What was the answer to the Sphinx's riddle in the thrid Tri-Wizard Task?",
+        answer: "Spider",
+        wrongs: ["Point Me","Boggart","Midnight"]
+    },
+    {
+        question: "Where is the headquarters of the Order of the Phoenix",
+        answer: "Number twelve, Grimmauld Place",
+        wrongs: ["Number four, Privet Drive","Tottenham Court Road","8 Heathgate, Hampstead Garden"]
+    },
+    {
+        question: "What was the row number in the Department of Mysteries where Harry's prophecy was stored?",
+        answer: "97",
+        wrongs: ["85", "131","76"]
+    },
+    {
+        question: "Which educational decree created the office of Hogwarts High Inquisitor?",
+        answer: "23",
+        wrongs: ["22", "24","25"]
+    },
 ];
